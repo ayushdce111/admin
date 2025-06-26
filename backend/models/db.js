c61a4fe1682b30650b37cb00ca80a65b70de09ea
unknown <ayushdce11@gmail.com>
2025-06-26 16:48:27 +0530

    add

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-// console.log(process.env.DB_Uri);
+console.log(process.env.DB_Uri);
 mongoose.connect(process.env.DB_Uri)
 .then(() =>{ console.log("connected DB")})
 .catch((err) =>{console.log("DB NOT CONNECTED",err)});
