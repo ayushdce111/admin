8ebbe250f1af16b632cd66037bb393c73780ae1d
unknown <ayushdce11@gmail.com>
2025-07-02 18:33:59 +0530

    add

--- frontend/axios.jsx
+++ frontend/axios.jsx
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "https://admin-backend-kf09.onrender.com",
-  // baseURL: "http://localhost:8000"
+  // baseURL: "https://admin-backend-kf09.onrender.com",
+  baseURL: "http://localhost:8000"
 });
 
 export default API;
